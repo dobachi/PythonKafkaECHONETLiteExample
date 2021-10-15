f6bf544216fe72ec3bd0965872e79feac7aea6bc
dobachi <dobachi1983oss@gmail.com>
2021-10-16 00:16:00 +0900

    Add packages

--- bin/jupyterlab.sh
+++ bin/jupyterlab.sh
@@ -2,4 +2,4 @@ export SPARK_HOME=venv/lib64/python3.7/site-packages/pyspark
 export PYSPARK_DRIVER_PYTHON="jupyter"
 export PYSPARK_DRIVER_PYTHON_OPTS="lab --ip 0.0.0.0"
 
-pyspark --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2,io.delta:delta-core_2.12:1.0.0 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
+pyspark --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2,io.delta:delta-core_2.12:1.0.0,org.apache.hadoop:hadoop-aws:3.2.0,io.delta:delta-sharing-spark_2.12:0.2.0 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" --conf "spark.hadoop.fs.s3a.aws.credentials.provider=com.amazonaws.auth.DefaultAWSCredentialsProviderChain"
